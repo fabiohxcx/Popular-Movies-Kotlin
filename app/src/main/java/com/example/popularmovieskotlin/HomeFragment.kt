f6812831018eb81a2f65dc822e437f81bee147a5
Fabio Lagoa <fabio.lagoa@bv.com.br>
2019-08-29 16:46:30 -0300

    Adding Data Binding

---
+++ app/src/main/java/com/example/popularmovieskotlin/HomeFragment.kt
@@ -0,0 +1,36 @@
+package com.example.popularmovieskotlin
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.example.popularmovieskotlin.databinding.HomeFragmentBinding
+
+
+class HomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding = HomeFragmentBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+
+    }
+
+}
--- app/src/main/java/com/example/popularmovieskotlin/MainActivity.kt
+++ app/src/main/java/com/example/popularmovieskotlin/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.popularmovieskotlin
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.AppCompatDelegate
-
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
