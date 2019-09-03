2f1ce3970a049ef91e2eccdb6384bdb5912220c3
Fabio Lagoa <fabio.lagoa@bv.com.br>
2019-09-03 15:09:25 -0300

    Adding Transformation for Release Date

--- app/src/main/java/com/example/popularmovieskotlin/detailmovie/DetailMovieViewModel.kt
+++ app/src/main/java/com/example/popularmovieskotlin/detailmovie/DetailMovieViewModel.kt
@@ -4,17 +4,23 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.example.popularmovieskotlin.model.Movie
 
-class DetailMovieViewModel(selectedMovie: Movie, app: Application) : AndroidViewModel(app) {
+class DetailMovieViewModel(selectedMovieArg: Movie, app: Application) : AndroidViewModel(app) {
 
     private val _selectedMovie = MutableLiveData<Movie>()
     val selectedMovie: LiveData<Movie>
         get() = _selectedMovie
 
+
     init {
-        _selectedMovie.value = selectedMovie
+        _selectedMovie.value = selectedMovieArg
     }
 
+    val displayReleaseDate = Transformations.map(selectedMovie) {
+        val array = selectedMovie.value!!.releaseDate?.split("-")
+        "${array.get(1)}/${array.get(2)}/${array.get(0)}"
+    }
 
 }
