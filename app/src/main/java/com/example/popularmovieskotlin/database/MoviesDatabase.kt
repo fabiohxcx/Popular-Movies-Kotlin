546bb413ac858281fe76f6c00406a32d7eb5e361
fabiohxcx <hideki.fabio@gmail.com>
2019-09-06 11:34:30 -0300

    removing all warnings

--- app/src/main/java/com/example/popularmovieskotlin/database/MoviesDatabase.kt
+++ app/src/main/java/com/example/popularmovieskotlin/database/MoviesDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [EntityMovie::class], version = 1)
+@Database(entities = [EntityMovie::class], version = 1, exportSchema = false)
 abstract class MoviesDatabase : RoomDatabase() {
     abstract val movieDao: MovieDao
 }
--- app/src/main/java/com/example/popularmovieskotlin/detailmovie/DetailMovieViewModelFactory.kt
+++ app/src/main/java/com/example/popularmovieskotlin/detailmovie/DetailMovieViewModelFactory.kt
@@ -7,8 +7,10 @@ import com.example.popularmovieskotlin.model.Movie
 
 class DetailMovieViewModelFactory(
     private val movie: Movie,
-    private val application: Application) : ViewModelProvider.Factory {
+    private val application: Application
+) : ViewModelProvider.Factory {
 
+    @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(DetailMovieViewModel::class.java)) {
             return DetailMovieViewModel(movie, application) as T
